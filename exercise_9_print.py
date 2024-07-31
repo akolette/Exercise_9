d748209831c2c6e74e9598623deff8390cca18d0
Andy Ko <andy@ko-family.org>
2024-07-31 01:33:43 -0400

    revised exercise_9_print.py

---
+++ exercise_9_print.py
@@ -0,0 +1,4 @@
+""" After you clone your GitHub repository, create a Python script in the directory of your repository. 
+Add a simple function to the script (for example, a function that prints "hello")."""
+
+print("hello world")
